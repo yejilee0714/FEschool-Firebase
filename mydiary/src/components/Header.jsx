7d4c4ee9fc05a6db9f37635ff8528ed95a873d3c
yejilee0714 <ellie.yejilee@gmail.com>
2023-06-08 19:15:49 +0900

    firebase활용 - diary

---
+++ mydiary/src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import styles from './Header.module.css'
+import { Link } from 'react-router-dom'
+import logo from '../img/logo.svg'
+import { useLogout } from '../hooks/useLogout.js'
+
+export default function Header() {
+    const { logout } = useLogout();
+
+
+    return (
+        <header>
+            <div className={styles["header-wrap"]}>
+                <h1>
+                    <Link to="/">
+                        <img className="logo" src={logo} alt="두근두근 비밀일기" />
+                    </Link>
+                </h1>
+                <div>
+                    <Link to="/signup" className="btn-join">회원가입</Link>
+                    <Link to="/" className="btn-logout" onClick={logout} >로그아웃</Link>
+                </div>
+            </div>
+        </header>
+    )
+}
